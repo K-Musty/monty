3502cd763a4d759a96030f96debfd75859f2e83e
K-Musty <kmustapha9564@gmail.com>
2024-01-21 12:14:20 -0800

    commit

---
+++ 11-cool_func.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * cool_rotr - Function to rotate to bottom
+ * @stack: A pointer to the top node
+ * @line_number: Line number
+ */
+void __our_monty_rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *bottom, *top;
+
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+		return;
+
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
+
+	bottom->prev->next = NULL;
+	(*stack)->next = bottom;
+	bottom->prev = *stack;
+	bottom->next = top;
+	top->prev = bottom;
+
+	(void)line_number;
+}
+
+/**
+ * cool_rotl - Rotates the top to bottom
+ * @stack: A pointer to the top node
+ * @line_number: line number
+ */
+void cool_rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *bottom, *top;
+
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+		return;
+
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
+
+	top->next->prev = *stack;
+	(*stack)->next = top->next;
+	bottom->next = top;
+	top->next = NULL;
+	top->prev = bottom;
+
+	(void)line_number;
+}
