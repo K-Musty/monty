868cb2a7da62aa166d4509181e1d356bf6f7b0f1
K-Musty <kmustapha9564@gmail.com>
2024-01-21 12:04:40 -0800

    commit

---
+++ 10-stack_queue.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * __our_monty_stack - function to change queue to a stack.
+ * @stack: A pointer to top node
+ * @line_number: line number
+ */
+void cool_mon_stack(stack_t **stack, unsigned int line_number)
+{
+	(*stack)->n = STACK;
+	(void)line_number;
+}
+
+/**
+ * cool_mon_queue -function to change a stack to a queue.
+ * @stack: A pointer to the top node
+ * @line_number: line number
+ */
+void cool_mon_queue(stack_t **stack, unsigned int line_number)
+{
+	(*stack)->n = QUEUE;
+	(void)line_number;
+}
+
